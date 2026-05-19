886f22c4f779402f7a31c26437209751e4ec41ad
shubham Yadav <sy907219@gmail.com>
2026-05-19 15:58:26 +0530

    Model code and relation of review

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -1,10 +1,27 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    protected $fillable = [
+        'product_id', 'user_id', 'order_item_id',
+        'rating', 'title', 'comment', 'is_approved'
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function orderItem()
+    {
+        return $this->belongsTo(OrderItem::class);
+    }
 }
